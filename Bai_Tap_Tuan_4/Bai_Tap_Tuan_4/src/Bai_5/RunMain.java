8234daa4aa74dbaf9e5573e0381178cdec8bf704
Nguyễn Quý <79324711+nguyenxuanquy10@users.noreply.github.com>
2021-11-07 10:12:21 +0700

    Update RunMain.java

--- Bai_Tap_Tuan_4/Bai_Tap_Tuan_4/src/Bai_5/RunMain.java
+++ Bai_Tap_Tuan_4/Bai_Tap_Tuan_4/src/Bai_5/RunMain.java
@@ -26,7 +26,7 @@ public class RunMain {
         return true; 
      }
      public static boolean isPrime(double n){
-       // if((int)n!=n) return false; 
+        if((int)n!=n) return false; 
         if(n<1) return false; 
         for(int i=2;i*i<=n;i++){
             if(n%i==0) return false; 
